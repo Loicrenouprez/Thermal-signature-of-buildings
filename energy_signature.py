a8dc860cd2083df9b14508bd1486bb8c5490c548
loicrenouprez <renouprezl@gmail.com>
2023-10-29 23:55:58 +0100

    Energy signature more complete code
    Using the previous version of the code, different functions have been added to be more general. The treatment of the data of weather and a plot of the energy signature are also developped (still an error in the function treat data due to missing hours in the data file not good taken into account to fill the array data hour by hour)

--- energy_signature.py
+++ energy_signature.py
@@ -7,45 +7,93 @@ Created on Sun Oct 29 16:57:38 2023
 
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
 
-file_path = "donnes_chaleur/B67_CHA_RC_ECS.csv"
 
-file = open(file_path, 'r')
-csv_reader = csv.reader(file)
+def nb_lines(file_path):
+    file = open(file_path, 'r')
+    csv_reader = csv.reader(file)
+    line_count = sum(1 for row in csv_reader)
+    file.close()
+    return line_count
 
-line_count = sum(1 for row in csv_reader)
-file.close()
-file = open(file_path, 'r')
-csv_reader = csv.reader(file, delimiter =';')
-rows = []
-for row in csv_reader:
-    cell_array = []
-    for cell in row:
-        cell_array.append(cell)  
-    rows.append(cell_array)
+def treat_data(file_path, line_count):
+   
+    file = open(file_path, 'r')
+    csv_reader = csv.reader(file, delimiter =';')
+    rows = []
+    for row in csv_reader:
+        cell_array = []
+        for cell in row:
+            cell_array.append(cell)  
+        rows.append(cell_array)
+        
+    nb_measure = 0
+    data = np.zeros((8760), dtype = float)
+    a = 0.0
+    hour = 0
+    for i in range(1, line_count):
+        if i == 1 :
+            a += float(rows[i][2].replace(',', '.'))
+            nb_measure += 1
+            continue
+        if (int(rows[i][0][9:11])) != int((rows[i-1][0][9:11])) :
+            #to handle the case of missing hours
+            if abs((int(rows[i][0][9:11])) - int((rows[i-1][0][9:11]))) > 1 :
+                nb_missing = (int(rows[i][0][9:11])) - int((rows[i-1][0][9:11]))
+                for j in range(nb_missing) :
+                    data[hour] = float(rows[i-1][2].replace(',', '.'))
+                    hour += 1                       
+            data[hour] = a/nb_measure
+            nb_measure = 1
+            a = 0.0
+            hour += 1
+        else :
+            a += float(rows[i][2].replace(',', '.'))
+            nb_measure += 1
+        if i == (line_count-1):
+            data[hour] = a/nb_measure
+    file.close()
+    return data
 
-  
-nb_measure = 0
-data = np.zeros((8760), dtype = float)
+def treat_weather_data(file_path, nb_lines):
+    file = open(file_path, 'r')
+    csv_reader = csv.reader(file, delimiter =';')
 
-a = 0.0
-date = 0
-for i in range(1, 578):
-    if i == 1 :
-        a += float(rows[i][2].replace(',', '.'))
-        nb_measure += 1
-        continue
-    if (rows[i][0][8:10]) != (rows[i-1][0][8:10]) :
-        print(i)
-        data[date] = (a)/(nb_measure)
-        nb_measure = 0
-        a = 0.0
-        date += 1
-    else :
-        a += float(rows[i][2].replace(',', '.'))
-        nb_measure += 1
-    if i == (line_count-1):
-        data[date] = a/nb_measure
+    temp_array = []
+    i = 0
+    j = 0
+    for row in csv_reader:
+        #to avoid having the title of the column in the array
+        if i == 0 :
+            i +=1
+            j = 0
+            continue
+        if len(row) > 4:
+            temperature = row[4]
+            temp_array.append(temperature)
+            temp_array[j] = float(temp_array[j])
+        j+=1
 
-print(data)
+    return temp_array
 
+def create_figure(heat_data, temperature_data):
+    plt.scatter(temperature_data, heat_data, marker = 'o')
+    min_temp = np.min(temperature_data)
+    max_temp = np.max(temperature_data)
+    print(int((max_temp-(-min_temp))/10))
+    new_x_ticks = np.linspace(min_temp, max_temp, num = 10)
+    print(new_x_ticks)
+    plt.xticks(new_x_ticks)
+    print(1)
+    plt.xlabel('Temperature')
+    plt.ylabel('Power supply [kW]')
+    return 
+
+if __name__ == "__main__":
+    file_path1 = "donnes_chaleur/B67_CHA_RC_ECS.csv"
+    file_path2 = "Weather_data_Solcast.csv"
+    nb_lines = nb_lines(file_path1)
+    heat_data = treat_data(file_path1, nb_lines)
+    temperature_data = treat_weather_data(file_path2, 8761)
+    create_figure(heat_data, temperature_data)
